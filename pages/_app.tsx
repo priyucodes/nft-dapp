3b8d4f4307dda81acaa634497a8868e0a8216bd6
Priyanshu Bhandari <technopriyu@gmail.com>
2022-10-06 13:26:35 +0530

    built login page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  // Rinkeby is known as Test network for Ethereum. Testing payments with fake Money.
+  return (
+    <ThirdwebProvider desiredChainId={ChainId.Rinkeby}>
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
 }
 
 export default MyApp;
